3507bb8e45e6317e61a87228ed07854518cd8926
efimovandrei <94910824+efimovandrei@users.noreply.github.com>
2022-02-05 15:13:30 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -6,11 +6,14 @@ import requests
 
 LON = '48.566888'
 LAT = '55.856320'
+zoom = 19
 api_server = 'http://static-maps.yandex.ru/1.x/'
 par = {
     "ll": ",".join([LON, LAT]),
-    "spn": ",".join(['0.001', '0.001']),
-    "l": "map"}
+
+    "l": "map",
+    'z': str(zoom)
+}
 
 response = requests.get(api_server, params=par)
 
@@ -29,11 +32,25 @@ with open(map_file, "wb") as file:
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 # Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
 # Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                print(1)
+                if zoom + 1 <= 19:
+                    zoom += 1
+                    response = requests.get(api_server, params=par)
+                    map_file = "map.png"
+                    with open(map_file, "wb") as file:
+                        file.write(response.content)
+
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    # Переключаем экран и ждем закрытия окна.
+    pygame.display.flip()
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
